1940d74f84134bded3d5b027502e2e2a065da715
180101061 <45733666+180101061@users.noreply.github.com>
2019-08-20 21:40:23 +0530

    Update tree.cpp

--- lab_4/tree.cpp
+++ lab_4/tree.cpp
@@ -1,202 +1,203 @@
 #include<bits/stdc++.h>
+using namespace std;
 
-using namespace std ;
-//checking precedence
-int pre(char chr) 
+
+struct et 
 { 
-    if(chr == '^') 
+    string value; 
+    et* left, *right; 
+}; 
+
+int prec(string c) 
+{ 
+    if(c == "^") 
     return 3; 
-    else if(chr == '*' || chr == '/') 
+    else if(c == "*" || c == "/") 
     return 2; 
-    else if(chr == '+' || chr == '-') 
+    else if(c == "+" || c == "-") 
     return 1; 
     else
     return -1; 
 } 
-//converting infix expression to 
-//postfix expression
-stack<int> infixToPostfix(string s) 
+
+
+bool isOperator(string str) 
 { 
-    stack<char> str; 
-    str.push('N'); 
-    
-    stack<int> ns; 
-    for(int i = 0; i < s.length(); i++) 
+    if (str.length()==1 && (str == "+" || str == "-" || 
+            str == "*" || str == "/" || 
+            str == "^") )
+        return true; 
+    return false; 
+} 
+
+et* newNode(string v) 
+{ 
+    et *temp = new et; 
+    temp->left = temp->right = NULL; 
+    temp->value = v; 
+    return temp; 
+}; 
+  
+
+vector<string> postfix(vector<string> s) 
+{ 
+    std::stack<string> st; 
+    st.push("N"); 
+    int l = s.size(); 
+    vector<string> ns; 
+    for(int i = 0; i < l; i++) 
     { 
          
-        if((s[i] >= '0' && s[i] <= '9')) 
-        ns+=s[i]; 
-  
+        if(!isOperator(s[i]) && s[i]!="" && s[i]!="(" && s[i]!=")") 
+         ns.push_back(s[i]); 
         
-        else if(s[i] == '(') 
+  
+       
+        else if(s[i] == "(")   
+        st.push("("); 
           
-        str.push('('); 
-           
-        else if(s[i] == ')') 
+       
+        else if(s[i] == ")") 
         { 
-            while(str.top() != 'A' && str.top() != '(') 
+            while(st.top() != "N" && st.top() != "(") 
             { 
-                char c = str.top(); 
-                str.pop(); 
-               ns += c; 
+                string c = st.top(); 
+                st.pop(); 
+
+              if(c!="" && c!="(" && c!=")")ns.push_back(c);
+               
             } 
-            if(str.top() == '(') 
+            if(st.top() == "(") 
             { 
-                char c = str.top(); 
-                str.pop(); 
+                string c = st.top(); 
+                st.pop();  
             } 
         } 
           
         
         else{ 
-            while(str.top() != 'A' && pre(s[i]) <= pre(str.top())) 
+            while(st.top() != "N" && prec(s[i]) <= prec(st.top())) 
             { 
-                char c = str.top(); 
-                str.pop(); 
-                ns += c; 
+                if(s[i]=="^"&&st.top()=="^") break;
+                string c = st.top(); 
+                st.pop(); 
+                if(c!="" && c!="(" && c!=")")ns.push_back(c); 
+                
             } 
-            str.push(s[i]); 
+            st.push(s[i]); 
         } 
   
-    }
-    while(str.top() != 'A') 
+    } 
+  
+    while(st.top() != "N") 
     { 
-        char c = str.top(); 
-        str.pop(); 
-        ns += c; 
+                string c = st.top(); 
+                st.pop(); 
+              if(c!="" && c!="(" && c!=")") ns.push_back(c); 
     } 
       
     return ns;
   
 } 
-struct et 
+
+
+et* constructtree(vector<string> postfix) 
 { 
-    int i; 
-    et* left, *right, *root; 
-};
-bool isOperator(char c) 
-{ 
-    if (c == '+' || c == '-' || 
-            c == '*' || c == '/' || 
-            c == '^') 
-        return true; 
-    return false; 
-} 
-et* newNode(int v) 
-{ 
-    et *temp = new et; 
-    temp->left = temp->right = NULL; 
-    temp->i = v; 
-    return temp; 
-};
-et* constructTree(stack<int> s) 
-{ 
-    stack<et *> st; 
+    stack<et *> str; 
     et *t, *t1, *t2; 
   
- 
-    while(!s.empty())
+   
+    for (int i=0; i<postfix.size(); i++) 
+    { 
+       
+        if (!isOperator(postfix[i])) 
+        { 
+            t = newNode(postfix[i]); 
+            str.push(t); 
+        } 
+        else 
+        { 
+            t = newNode(postfix[i]); 
+  
+          
+            t1 = str.top();
+            str.pop();      
+            t2 = str.top(); 
+            str.pop(); 
+  
+          
+            t->right = t1; 
+            t->left = t2; 
+  
+          
+            str.push(t); 
+        } 
+    } 
+  
+    
+    t = str.top(); 
+    str.pop(); 
+  
+    return t; 
+} 
+int eval(et * root){
+    if(root==NULL) return 0;
+    if(root->left==NULL&&root->right==NULL){
+        return stoi(root->value);
+    }
+    int left_val=eval(root->left);
+    int right_val=eval(root->right);
+    if(root->value=="+") return left_val+right_val;
+    else if(root->value=="-") return left_val-right_val;
+    else if(root->value=="*") return left_val*right_val;
+    else if(root->value=="/") return left_val/right_val;
+    else return pow(left_val,right_val);
+}
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
     {
-        if(s.top()>=0)
+        int l;
+        cin>>l;
+     while(l--)
         {
-            t=newNode(s.top());
-            s.pop();
-            st.push(t);
+         string s;
+    cin>>s;
+    vector<string> str;
+    string temp="";
+    
+    for(int i=0; i<s.length();i++)
+    {
+        
+        if(s[i]>=48 && s[i]<=57)
+        {
+          temp+=s[i];
         }
         else
         {
-            t=newNode(s.top());
-            t1=st.top();
-            st.pop();
-            t2=st.top();
-            st.pop();
-
-            t->right=t1;
-            t->left=t2;
-
-            st.push(t);
-            s.pop();
+            if(temp=="" && s[i]=='-')
+                {temp+=s[i]; continue;}
+             if(temp!="")str.push_back(temp); 
+            temp=""; temp+=s[i]; 
+              if(temp!="")str.push_back(temp);
+            temp="";
         }
+
     }
-    return t; 
+    
+    str.push_back(temp);
+    
+     vector<string> postexp=postfix(str);
   
     
-} 
-int toInt(string s)  
-{  
-    int num = 0;  
-      
-     
-    if(s[0]!='-') 
-        for (int i=0; i<s.length(); i++)  
-            num = num*10 + (int(s[i])-48);  
-   
-    else
-        for (int i=1; i<s.length(); i++)  
-        { 
-            num = num*10 + (int(s[i])-48);  
-            num = num*-1; 
-        } 
-      
-    return num;  
-} 
 
-
-int eval(et* root)  
-{  
-     
-    if (!root)  
-        return 0;  
-  
-    
-    if (!root->left && !root->right)  
-        return root->i;  
-  
-    
-    int l_val = eval(root->left);  
-  
-    
-    int r_val = eval(root->right);  
-  
-     
-    if (root->i=='+')  
-        return l_val+r_val;  
-  
-    if (root->i=='-')  
-        return l_val-r_val;  
-  
-    if (root->i=='*')  
-        return l_val*r_val;  
-
-    if(root->i=='/')
-        return l_val/r_val;
-    return pow(l_val,r_val);  
-}  
-int main()
-{   
-    int q;
-    cin>>q;
-    while(q--)
-    {
-        int T;
-        cin>>T;
-        while(T--)
-        {
-            char s[100000];
-            cin>>s;
-           
-            stack <int> S,s1;
-            S=infixToPostfix(s);
-            while(!S.empty())
-            {
-                s1.push(S.top());
-                S.pop();
-            }
-            
-           et* p=constructTree(s1);
-           
-            cout<<eval(p)<<endl;
-           
-        }
+    struct et* root=constructtree(postexp);
+    int eval_value = eval(root);
+    cout<<eval_value<<endl;
     }
+    }
+return 0;
 }
